615f4d1018a6056c33a6349a5d8813e20bbf78d1
CVR <24b81a6705cvr.ac.in>
2026-03-12 14:27:09 +0530

    javacode

---
+++ JavaDemo.java
@@ -0,0 +1,15 @@
+	class JavaDemo{
+		 
+		int sum(int x,int y){
+				return x+y;
+			}
+		int sub(int x,int y){
+				return x-y;
+			}
+		public static void main(String[] args){
+		JavaDemo  obj = new JavaDemo();
+			System.out.println("sum of two numbers:"+obj.sum(4,5));
+			System.out.println("sub of two numbers:"+obj.sub(7,5));
+		}
+	}
+						
